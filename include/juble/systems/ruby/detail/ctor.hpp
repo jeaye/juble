b781e3e9b787b5ad6a7c2e145ea0eacec685e10a
jeaye <contact@jeaye.com>
2014-07-27 01:42:56 +0800

    Refactored ruby detail into more headers

---
+++ include/juble/systems/ruby/detail/ctor.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+namespace script
+{
+  namespace ruby_detail
+  {
+    template <typename F>
+    class ctor_wrapper;
+    template <typename Class, typename... Args>
+    class ctor_wrapper<Class (Args...)> final
+    {
+      public:
+        ctor_wrapper() = delete;
+        ctor_wrapper(std::function<Class* (Args...)> const &func)
+        { func_ = func; }
+
+        static value_type call(int const argc, value_type * const argv, value_type const self)
+        {
+          using uptr_t = std::unique_ptr<detail::bare_t<Class>>;
+
+          juble_assert(func_, "invalid ctor call");
+          juble_assert(sizeof...(Args) == argc, "invalid argument count");
+
+          auto * const data(new uptr_t
+          { call_impl(std::index_sequence_for<Args...>{}, argv) });
+
+          auto const deleter([](void * const data)
+          { delete static_cast<uptr_t*>(data); });
+
+          value_type const obj{ Data_Wrap_Struct(self, nullptr, deleter, data) };
+          return obj;
+        }
+
+      private:
+        template <size_t... Ns>
+        static Class* call_impl(std::index_sequence<Ns...> const,
+                                value_type * const argv)
+        {
+          (void)argv; /* XXX: When Ns is 0, argv is not used. */
+          return func_(from_ruby<Args>(argv[Ns])...);
+        }
+
+        static std::function<Class* (Args...)> func_;
+    };
+    template <typename Class, typename... Args>
+    std::function<Class* (Args...)> ctor_wrapper<Class (Args...)>::func_;
+  }
+}
