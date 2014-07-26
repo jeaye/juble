bb45607f42522ec29334800a7816da3dfe476b6d
jeaye <contact@jeaye.com>
2014-07-27 01:06:31 +0800

    Multiple ruby functions of the same type: check

--- test/include/generic/function/non_member.hpp
+++ test/include/generic/function/non_member.hpp
@@ -23,7 +23,10 @@ namespace script
   non_member_group const non_member_obj{ "non-member" };
 
   template <typename System>
-  char constexpr const * call_nullary()
+  char constexpr const * call_nullary_1()
+  { return ""; }
+  template <typename System>
+  char constexpr const * call_nullary_2()
   { return ""; }
 }
 
@@ -34,21 +37,29 @@ namespace jest
 {
   namespace detail
   {
-    void nullary()
+    void nullary_1()
     { std::cout << "excellent"; }
+    void nullary_2()
+    { std::cout << "\nlines\n"; }
   }
 
   template <> template <>
   void script::non_member_group::test<0>() /* nullary function */
   {
-    script::registrar::add(script::func(&detail::nullary, "nullary"));
+    script::registrar::add(script::func(&detail::nullary_1, "nullary_1"));
+    script::registrar::add(script::func(&detail::nullary_2, "nullary_2"));
 
     clear();
     detail::each_system([&](auto tag)
     {
-      script::system<decltype(tag)>::eval(script::call_nullary<decltype(tag)>());
+      script::system<decltype(tag)>::eval(script::call_nullary_1<decltype(tag)>());
       expect_equal(out.str(), "excellent");
-      this->clear(); /* XXX: GCC bug requires this-> in generic lambda. */
+
+      this->clear();
+      script::system<decltype(tag)>::eval(script::call_nullary_2<decltype(tag)>());
+      expect_equal(out.str(), "\nlines\n");
+
+      this->clear();
     });
   }
 }
