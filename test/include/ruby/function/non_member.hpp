02173dca566b1b6d0c0ff3df3f8a07e21a4ff8c5
jeaye <contact@jeaye.com>
2014-07-27 13:20:49 +0800

    Added unary function tests

--- test/include/ruby/function/non_member.hpp
+++ test/include/ruby/function/non_member.hpp
@@ -8,4 +8,11 @@ namespace script
   template <>
   char constexpr const * call_nullary_2<ruby_tag>()
   { return "nullary_2();"; }
+
+  template <>
+  char constexpr const * call_unary_1<ruby_tag>()
+  { return "unary_1(42);"; }
+  template <>
+  char constexpr const * call_unary_2<ruby_tag>()
+  { return "unary_2(\"string\");"; }
 }
