38536b45d78c69a6c66453d4660aace2bb5efc4d
jeaye <contact@jeaye.com>
2014-07-28 18:17:40 +0800

    juble_assert type safety

--- include/juble/detail/assert.hpp
+++ include/juble/detail/assert.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <cassert>
-#include <stdexcept>
 
-#define juble_assert_failure(msg) \
-  std::cerr << "assertion failure: " << (msg) << std::endl; \
-  assert(false)
-//#define juble_assert_failure(msg) throw std::runtime_error{ msg }
-#define juble_assert(test, msg) \
-  if(!(test))\
-  { juble_assert_failure(msg); }
+namespace script
+{
+  template <typename T>
+  void juble_assert(T const &test, char const * const str)
+  {
+    if(!static_cast<bool>(test))
+    {
+      std::cerr << "assertion failure: " << str << std::endl;
+      assert(false);
+    }
+  }
+  template <typename T>
+  void juble_assert(T const &test, std::string const &str)
+  { juble_assert(static_cast<bool>(test), str.c_str()); }
+}
